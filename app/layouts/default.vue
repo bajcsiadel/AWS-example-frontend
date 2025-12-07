1c83448bfd61c583f9294d465830ace5a76339c1
Adél Bajcsi <bajcsiadel@gmail.com>
2025-12-07 23:35:36 +0200

    added files

---
+++ app/layouts/default.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+/* data */
+const darkCssSelector: string = "dark-theme";
+
+/* computed */
+const isDark: ComputedRef<boolean> = computed(() => document?.documentElement?.classList?.contains(darkCssSelector) ?? false)
+
+/* methods */
+function toggleDarkMode() {
+    document.documentElement.classList.toggle(darkCssSelector);
+}
+</script>
+
+<template>
+    <div class="tw:h-screen tw:flex tw:flex-col tw:justify-between tw:bg-white tw:dark:bg-green-700">
+        <nav id="navigation-bar" class="tw:bg-green-400 tw:py-5 tw:shadow-md tw:dark:bg-green-950">
+          <div class="tw:flex tw:justify-between container">
+            <router-link to="/" class="tw:text-2xl tw:text-black tw:dark:text-green-400 tw:hover:cursor-pointer tw:hover:underline tw:transition-all">My TODO list</router-link>
+            <div class="tw:flex tw:flex-nowrap tw:items-center tw:justify-center tw:gap-x-1">
+              <i class="fa-solid fa-moon tw:text-black tw:dark:text-green-400"></i>
+              <prime-toggle-switch @change="toggleDarkMode" :default-value="!isDark" />
+              <i class="fa-regular fa-sun tw:text-black tw:dark:text-green-400"></i>
+            </div>
+          </div>
+        </nav>
+        <div id="main-content" class="container tw:mt-6 tw:grow">
+            <slot></slot>
+        </div>
+        <footer id="footer" class="tw:bg-black tw:py-5 tw:text-center tw:text-white">
+            &#169; by Adél Bajcsi as AWS exercise
+        </footer>
+    </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,50 @@
+
+import Aura from '@primeuix/themes/Aura';
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: [
+    "@formkit/auto-animate/nuxt",
+    "@primevue/nuxt-module",
+  ],
+  app: {
+    head: {
+      script: [
+        {
+          src: "https://kit.fontawesome.com/092629ea56.js",
+          crossorigin: "anonymous"
+        }
+      ]
+    }
+  },
+  runtimeConfig: {
+    public: {
+      API_URL: `${process.env.API_HOST}:${process.env.API_PORT}`
+    }
+  },
+  css: ['@/assets/css/main.css'],
+  primevue: {
+    components: {
+      prefix: "Prime"
+    },
+    directives: {
+      prefix: "Prime"
+    },
+    options: {
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: "dark-theme"
+        }
+      },
+    },
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+})
